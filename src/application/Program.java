7862a121ec16040173bc291e72e95a3f59d15120
DenysFerreira <lerroandeco@gmail.com>
2020-02-06 23:45:39 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,6 +33,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 
 }
